121c7601e37060f9bb3d4a9503b292511a1696dd
eldanaomushova <eldana956@gmail.com>
2024-02-13 19:12:27 +0600

    slider changed added

--- src/components/MainPageSection.js
+++ src/components/MainPageSection.js
@@ -1,66 +1,29 @@
 import React from 'react';
-import HeroSlider, { Slide } from 'hero-slider';
-import im1 from '../images/for children/Babies.jpg';
-import im2 from '../images/for_man/Calvin Klein CK BE 200ml EDT.jpg';
-import im3 from '../images/for hair/Beste™ No_ 9 Jelly Cleanser - Drunk Elephant | Sephora.jpg';
-import im4 from '../images/for_body/Body and Hair Care Products_ Japanese Cosmetics Ranking 2020 Mid-Year.jpg';
-import im5 from '../images/for_skin/ -3.jpg';
-import im6 from '../images/boxes/Function of Beauty Packaging.jpg';
+import { Fade } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import im1 from '../images/logo/im1.jpg'
+
+
+import '../styles/MainPageSection.css'
+
+const fadeImages = [
+    {
+        url: im1
+    }
+    
+];
 
 const MainPageSection = () => {
     return (
-        <div className='mainpage-container'>
-            <img src={im1} alt="Babies" />
-            <HeroSlider
-                slidingAnimation="left_to_right"
-                orientation="horizontal"
-                initialSlide={0}
-                settings={{
-                    slidingDuration: 500,
-                    slidingDelay: 2000,
-                    shouldAutoplay: true,
-                    shouldDisplayButtons: true,
-                    autoplayDuration: 5000,
-                    height: "30vh",
-                }}
->
-                <Slide
-                    background={{
-                        backgroundImage: `url(${im1})`,
-                        backgroundAttachment: ""
-                    }}
-                />
-                <Slide
-                    background={{
-                        backgroundImage: `url(${im2})`,
-                        backgroundAttachment: ""
-                    }}
-                />
-                <Slide
-                    background={{
-                        backgroundImage: `url(${im3})`,
-                        backgroundAttachment: ""
-                    }}
-                />
-                <Slide
-                    background={{
-                        backgroundImage: `url(${im4})`,
-                        backgroundAttachment: ""
-                    }}
-                />
-                <Slide
-                    background={{
-                        backgroundImage: `url(${im5})`,
-                        backgroundAttachment: ""
-                    }}
-                />
-                <Slide
-                    background={{
-                        backgroundImage: `url(${im6})`,
-                        backgroundAttachment: ""
-                    }}
-                />
-            </HeroSlider>
+        <div className="slide-container">
+            <Fade>
+                {fadeImages.map((fadeImage, index) => (
+                    <div key={index} className='image-container'>
+                        <img style={{ width: '100%' }} src={fadeImage.url} alt={fadeImage.caption} />
+                    
+                    </div>
+                ))}
+            </Fade>
         </div>
     );
 };
