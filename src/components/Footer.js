80b9fb66c9190bc7bc6562130330bf3309608c7c
eldanaomushova <eldana956@gmail.com>
2024-02-18 18:08:44 +0600

    Footer finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../styles/Slider.css'; 
+import '../styles/Footer.css'; 
 import im1 from '../images/slider_images/1.jpg';
 import im2 from '../images/slider_images/2.jpg';
 import im3 from '../images/slider_images/3.jpg';
@@ -9,6 +9,7 @@ import im6 from '../images/slider_images/6.jpg';
 import im7 from '../images/slider_images/7.jpg';
 import im8 from '../images/slider_images/8.jpg';
 import im9 from '../images/slider_images/9.jpg';
+import logo1 from '../images/logo/logo1.png'
 
 const Footer = () => {
     const images = [im1, im2, im3, im4, im5, im6, im7, im8, im9];
@@ -33,6 +34,30 @@ const Footer = () => {
                         alt={`Slider Image ${index}`}
                     />
                 ))}
+            </div>
+            <div className='footer-container'>
+                <div className='logo-quotage'>
+                    <img src={logo1} alt='logo'/>
+                    <blockquote>"Your beauty starts with StellerPi</blockquote>
+                </div>
+                <div className='footer'>
+                    <div className='socials'>
+                        <p>Follow us on social media</p>
+                        <span className='mdi mdi-instagram'></span>
+                        <span className='mdi mdi-facebook'></span>
+                        <span className='mdi mdi-twitter'></span>
+                        <span className='mdi mdi-youtube'></span>
+                    </div>
+                    <div className='info'>
+                        <p>E-mail</p>
+                        <a type='email'>help@gmail.com</a>
+                        <p>Call us</p>
+                        <a type='phone'>+996706060606</a>
+                    </div>
+                </div>
+                <div className='rights'>
+                    <p>All rights are reserved</p>
+                </div>
             </div>
         </div>
     );
